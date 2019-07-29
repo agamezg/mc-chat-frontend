0306afbd7e556ff09b4fd0afdf5749b6e7542579
ariel.gamez <L0g Sp!ral>
2019-07-28 23:22:23 -0300

    chapter_3

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./Header.scss";
+
+const Header = () => (
+    <div className="header">
+        <h2>Esto es un chat en tiempo real... cajeteala piola gatooo!!!</h2>
+    </div>
+);
+
+export default Header
+
